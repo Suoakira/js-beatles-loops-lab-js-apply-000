c222cdc3c21fbacdbdcc82faeaf0374ff8d481b8
Stephen James <steves0089@gmail.com>
2018-08-25 16:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,19 @@
 function theBeatlesPlay(musicians, instruments) {
   let emptyArray = [];
   for (let i = 0; i < musicians.length; i++) {
-    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`); // iterated over the two argument arrays 
   }
-  return emptyArray;
+  return emptyArray; // returns array after for loop has ran for musicians.length
   
   
+}
+
+function johnLennonFacts(facts) {
+  const newFacts = [];
+  let i = 0;
+  while (i < facts.length) { // will stop running when we get to i < facts.length
+    newFacts.push(`${facts[i]}!!!`); // pushs the new string to the newFacts array
+    i++;
+    
+  }
 }

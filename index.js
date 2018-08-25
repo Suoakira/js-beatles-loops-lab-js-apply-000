2e3cf1a1ca9e65623c7d2dea156e9ec5eca630bf
Stephen James <steves0089@gmail.com>
2018-08-25 16:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 function theBeatlesPlay(musicians, instruments) {
   let emptyArray = [];
   for (let i = 0; i < musicians.length; i++) {
-    emptyArray.push(`${musicians[i]} plays ${instruments[i]}.`);
-    return emptyArray;
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+    return emptyArray
   }
   
 }
